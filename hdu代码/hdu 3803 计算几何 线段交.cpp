a513172a83831cbce4e97b440d24ca9f42684bab
tianwaitian <1056572776@qq.com>
2017-09-23 20:46:59 +0800

    2017.9.23

--- hdu代码/hdu 3803 计算几何 线段交.cpp
+++ hdu代码/hdu 3803 计算几何 线段交.cpp
@@ -1,28 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+typedef __int64 ll;
 
-const double eps = 1e-8;
-const double PI = acos(-1);
-
-int gcd(int a,int b)
+int dcmp(ll x)
 {
-    return b==0 ? a : gcd(b,a%b);
+    if(x==0) return 0;
+    else if(x>0) return 1;
+    else return -1;
 }
 
-//三态函数，判断两个double在eps精度下的大小关系
-int dcmp(double x)
-{
-    if(fabs(x)<eps) return 0;
-    else
-        return x<0?-1:1;
-}
 struct Point{
-	double x,y;
-	double a,b,c,d;
-
+	ll x,y;
+    ll a,b,c,d;
 	Point (){}
-	Point(double _x,double _y)
+	Point(ll _x,ll _y)
 	{
 	    x = _x;
 	    y = _y;
@@ -40,66 +31,126 @@ struct Point{
 		return Point(x-b.x,y-b.y);
 	}
 	//点积
-	double operator *(const Point &b)const
+	ll operator *(const Point &b)const
 	{
 		return x*b.x + y*b.y;
 	}
 	//叉积
 	//P^Q>0,P在Q的顺时针方向；<0，P在Q的逆时针方向；=0，P，Q共线，可能同向或反向
-	double operator ^(const Point &b)const
+	ll operator ^(const Point &b)const
 	{
 		return x*b.y - b.x*y;
 	}
-	//排序用，按照x的坐标从小到大，如果x相同，那么按照y从小到大
-    bool operator <(const Point &b)const
-    {
-        return y<b.y||(y==b.y&&x<b.x);
-    }
 };
-typedef Point Vector;
-struct Line{
-    Point s,e;
-    double A,B,C;
-    Line(){}
-    Line(Point _s,Point _e)
+struct Frac{
+    ll q,p;
+
+    ll gcd(ll a,ll b)
     {
-        s = _s;
-        e = _e;
-        A = e.y-s.y;
-        B = s.x-e.x;
-        C = e^s;
-    }
-    //两直线相交求交点
-    //第一个值为0表示直线重合，为1表示平行，为2是相交
-    //只有第一个值为2时，交点才有意义
-    pair<int,Point> operator &(const Line &b)const
-    {
-        Point ans = Point();
-        if(dcmp((e-s)^(b.e-b.s)) == 0)
+        while(b)
         {
-            if(dcmp((b.e-s)^(b.e-b.s)) == 0)
-                return make_pair(0,ans);//重合
-            else
-                return make_pair(1,ans);//平行
+            ll tmp = a%b;
+            a = b;
+            b = tmp;
         }
-        ans.a = B*b.C-b.B*C;
-        ans.b = A*b.B-b.A*B;
-        ans.c = b.A*C-A*b.C;
-        ans.d = A*b.B-b.A*B;
-        return make_pair(2,ans);
+        return a;
+    }
+
+    void check(ll a,ll b)
+    {
+        ll tmp = gcd(a,b);
+        a /= tmp;
+        b /= tmp;
+        q = a;
+        p = b;
+        if(p<0)
+        {
+            p = -p;
+            q = -q;
+        }
     }
 };
-//判断线段A1A2和线段B1B2是否相交
-bool Segment_Intersect(Point A1,Point A2,Point B1,Point B2)
+
+bool IsPoint(Point a,Point b)
 {
-    if(max(A1.x,A2.x)>=min(B1.x,B2.x) &&
-       max(B1.x,B2.x)>=min(A1.x,A2.x) &&
-       max(A1.y,A2.y)>=min(B1.y,B2.y) &&
-       max(B1.y,B2.y)>=min(A1.y,A2.y) && //快速排斥实验
-       dcmp(((B1-A1)^(A2-A1))*((A1-B2)^(A2-A1)))>=0 && //跨立实验
-       dcmp(((A1-B1)^(B2-B1))*((B1-A2)^(B2-B1)))>=0)
-       return true;
+    if(a.x==b.x && a.y==b.y) return true;
     else return false;
+}
+
+//线段相交求交点 1代表相交 0代表不相交 -1代表交点无数
+pair<int,Point> segment_intersect_point(Point P1,Point P2,Point P3,Point P4)
+{
+    Point ans;
+    int k1,k2,k3,k4;
+    ll s1,s2,s3,s4;
+    k1 = dcmp(s1 = (P2-P1)^(P3-P1));
+    k2 = dcmp(s2 = (P2-P1)^(P4-P1));
+    k3 = dcmp(s3 = (P4-P3)^(P1-P3));
+    k4 = dcmp(s4 = (P4-P3)^(P2-P3));
+
+    //规范相交
+    if(k1^k2==-2 && k3^k4==-2)
+    {
+        ans.a = s1*P4.x-s2*P3.x;
+        ans.b = s1-s2;
+        ans.c = s1*P4.y-s2*P3.y;
+        ans.d = ans.b;
+        return make_pair(1,ans);
+    }
+    ll mi1,mi2,mx1,mx2;
+    if(P1.x!=P2.x)
+    {
+        mi1 = min(P1.x,P2.x);
+        mx1 = max(P1.x,P2.x);
+        mi2 = min(P3.x,P4.x);
+        mx2 = max(P3.x,P4.x);
+    }
+    else
+    {
+        mi1 = min(P1.y,P2.y);
+        mx1 = max(P1.y,P2.y);
+        mi2 = min(P3.y,P4.y);
+        mx2 = max(P3.x,P4.y);
+    }
+    if(k1==0 && k2==0 && k3==0 && k4==0)
+    {
+        if(mi2 < mi1 && mi1 < mx2 ||
+           mi2 < mx1 && mx1 < mx2 ||
+           mi1 < mi2 && mi2 < mx1 ||
+           mi1 < mx2 && mx2 < mx1 ||
+           mi2 <= mi1 && mx1 <= mx2 ||
+           mi1 <= mi2 && mx2 <= mx1)
+            return make_pair(-1,ans);
+    }
+    if(k1==0 && dcmp((P1-P3)*(P2-P3))<=0)
+    {
+        ans.a = P3.x;
+        ans.c = P3.y;
+        ans.b=ans.d=1;
+        return make_pair(1,ans);
+    }
+    if(k2==0 && dcmp((P1-P4)*(P2-P4))<=0)
+    {
+        ans.a = P4.x;
+        ans.c = P4.y;
+        ans.b=ans.d=1;
+        return make_pair(1,ans);
+    }
+    if(k3==0 && dcmp((P3-P1)*(P4-P1))<=0)
+    {
+        ans.a = P1.x;
+        ans.c = P1.y;
+        ans.b=ans.d=1;
+        return make_pair(1,ans);
+    }
+    if(k4==0 && dcmp((P3-P2)*(P4-P2))<=0)
+    {
+        ans.a = P2.x;
+        ans.c = P2.y;
+        ans.b=ans.d=1;
+        return make_pair(1,ans);
+    }
+    return make_pair(0,ans);
 }
 
 int main()
@@ -109,66 +160,61 @@ int main()
     while(t--)
     {
         Point P1,P2,P3,P4;
-        scanf("%lf %lf %lf %lf",&P1.x,&P1.y,&P2.x,&P2.y);
-        scanf("%lf %lf %lf %lf",&P3.x,&P3.y,&P4.x,&P4.y);
-        if(!Segment_Intersect(P1,P2,P3,P4))
+        scanf("%I64d %I64d %I64d %I64d",&P1.x,&P1.y,&P2.x,&P2.y);
+        scanf("%I64d %I64d %I64d %I64d",&P3.x,&P3.y,&P4.x,&P4.y);
+        if(IsPoint(P1,P2))
         {
-            printf("0\n");
-            continue;
-        }
-        Line L1 = Line(P1,P2),L2 = Line(P3,P4);
-        pair<int,Point> ans = L1&L2;
-        if(ans.first==0)
-        {
-            printf("INF\n");
+            if(IsPoint(P3,P4))
+            {
+                if(IsPoint(P1,P3))
+                {
+                    printf("1\n");
+                    printf("%I64d %I64d\n",P1.x,P1.y);
+                }
+                else
+                    printf("0\n");
+            }
+            else
+            {
+                if(dcmp((P3-P1)^(P4-P1))==0 && dcmp((P3-P1)*(P4-P1))<=0)
+                {
+                    printf("1\n");
+                    printf("%I64d %I64d\n",P1.x,P1.y);
+                }
+                else
+                    printf("0\n");
+            }
         }
         else
         {
-            printf("1\n");
-            int a,b,c,d;
-            a = (int)ans.second.a;
-            b = (int)ans.second.b;
-            c = (int)ans.second.c;
-            d = (int)ans.second.d;
-            if(a*b<0)
+            if(IsPoint(P3,P4))
             {
-                a = abs(a);
-                b = abs(b);
-                int tmp = gcd(a,b);
-                a /= tmp;
-                b /= tmp;
-                if(b>1) printf("-%d/%d ",a,b);
-                else printf("-%d ",a);
+                if(dcmp((P1-P3)^(P2-P3))==0 && dcmp((P1-P3)*(P2-P3))<=0)
+                {
+                    printf("1\n");
+                    printf("I64d %I64d\n",P3.x,P3.y);
+                }
+                else
+                    printf("0\n");
             }
             else
             {
-                a = abs(a);
-                b = abs(b);
-                int tmp = gcd(a,b);
-                a /= tmp;
-                b /= tmp;
-                if(b>1) printf("%d/%d ",a,b);
-                else printf("%d ",a);
-            }
-            if(c*d<0)
-            {
-                c = abs(c);
-                d = abs(d);
-                int tmp = gcd(c,d);
-                c /= tmp;
-                d /= tmp;
-                if(d>1) printf("-%d/%d\n",c,d);
-                else printf("-%d\n",c);
-            }
-            else
-            {
-                c = abs(c);
-                d = abs(d);
-                int tmp = gcd(c,d);
-                c /= tmp;
-                d /= tmp;
-                if(d>1) printf("%d/%d\n",c,d);
-                else printf("%d\n",c);
+                pair<int,Point> tmp = segment_intersect_point(P1,P2,P3,P4);
+                if(tmp.first==0)
+                    printf("0\n");
+                else if(tmp.first==-1)
+                    printf("INF\n");
+                else
+                {
+                    Frac ans1,ans2;
+                    ans1.check(tmp.second.a,tmp.second.b);
+                    ans2.check(tmp.second.c,tmp.second.d);
+                    printf("1\n");
+                    if(ans1.p==1) printf("%I64d ",ans1.q);
+                    else printf("%I64d/%I64d ",ans1.q,ans1.p);
+                    if(ans2.p==1) printf("%I64d\n",ans2.q);
+                    else printf("%I64d/%I64d\n",ans2.q,ans2.p);
+                }
             }
         }
     }
